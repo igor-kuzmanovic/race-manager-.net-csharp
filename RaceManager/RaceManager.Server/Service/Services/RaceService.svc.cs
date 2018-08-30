0b28bfebf00b84d8011cb315032ff9d86d6e210d
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-08-30 12:42:40 +0200

    Testing WCF basic authentication with a custom validator

--- RaceManager/RaceManager.Server/Service/Services/RaceService.svc.cs
+++ RaceManager/RaceManager.Server/Service/Services/RaceService.svc.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 using System.ServiceModel;
 using System.Text;
 
