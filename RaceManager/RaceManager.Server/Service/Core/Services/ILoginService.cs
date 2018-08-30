dfcd8e37bd96ccba74e8e6e913df3a71d99bfabd
igor.kuzmanovic.996 <igor.kuzmanovic.996@gmail.com>
2018-08-30 22:10:01 +0200

    Initial login service

---
+++ RaceManager/RaceManager.Server/Service/Core/Services/ILoginService.cs
@@ -0,0 +1,20 @@
+﻿using RaceManager.Server.Service.Core.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RaceManager.Server.Service.Core.Services
+{
+    [ServiceContract]
+    public interface ILoginService
+    {
+        [OperationContract]
+        string LogIn(string username, string password);
+
+        [OperationContract]
+        void LogOut(string securityToken);
+    }
+}
---
+++ RaceManager/RaceManager.Server/Service/Services/LoginService.svc.cs
@@ -0,0 +1,30 @@
+﻿using RaceManager.Server.DataAccess.Persistence;
+using RaceManager.Server.Service.Converters;
+using RaceManager.Server.Service.Core.DataTransferObjects;
+using RaceManager.Server.Service.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RaceManager.Server.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        public string LogIn(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+                return "1234";
+            return string.Empty;
+        }
+
+        public void LogOut(string securityToken)
+        {
+            if (securityToken == "1234")
+                return;
+        }
+    }
+}
